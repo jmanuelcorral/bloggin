aa852fecf227510569195539d9bb1eda0f91e332
jmanuelcorral <jose.manuel.corral@gmail.com>
2012-09-16 23:24:51 +0200

    new version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloggin::Application.config.secret_token = '386f481aaafb78ee7439c64a1cb76612906f197b291886612b23b75deb6b667bfc469c5a8488b0f13ae4db1b3229c288b77789c3f33561365fa2c1f2eff069f6'
